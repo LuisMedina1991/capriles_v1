4c2983f3da509dbd1b89e90d39f1a83d1cd3a546
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-17 14:27:09 -0400

    formulario de bancos version final

--- resources/views/livewire/bank/form.blade.php
+++ resources/views/livewire/bank/form.blade.php
@@ -1,72 +1,55 @@
 @include('common.modalHead')
-<div class="row">
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Nombre</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="name" class="form-control component-name"
-                    placeholder="Nombre para el banco...">
-            </div>
-            @error('name')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Alias</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="alias" class="form-control" placeholder="Alias para el banco...">
-            </div>
-            @error('alias')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Codigo de Entidad</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="number" wire:model.lazy="entity_code" class="form-control" placeholder="1234">
-            </div>
-            @error('entity_code')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
 
-    @if($selected_id > 0 && $search_2 > 0)
-
-        <div class="col-sm-6">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Nombre*</b></label>
             <div class="form-group">
-                <label><b>Estado del registro</b></label>
-                <select wire:model="status_id" class="form-control text-uppercase">
-                    <option value="elegir">elegir</option>
-                    <option value="1">activo</option>
-                    <option value="2">bloqueado</option>
-                </select>
-                @error('status_id')
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="name" class="form-control component-name"
+                        placeholder="Nombre para el banco...">
+                </div>
+                @error('name')
                 <span class="text-danger er">{{ $message }}</span>
                 @enderror
             </div>
         </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Alias*</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="alias" class="form-control" placeholder="Alias para el banco...">
+                </div>
+                @error('alias')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Codigo*</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="number" wire:model.lazy="entity_code" class="form-control" placeholder="Codigo de entidad...">
+                </div>
+                @error('entity_code')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+    </div>
 
-    @endif
-
-</div>
 @include('common.modalFooter')
