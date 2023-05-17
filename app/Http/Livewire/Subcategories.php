cb88edde1e2a66ec8d192887c394546713231d26
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-17 13:39:37 -0400

    controlador de subcategorias con relaciones inciales

--- app/Http/Livewire/Subcategories.php
+++ app/Http/Livewire/Subcategories.php
@@ -7,13 +7,13 @@ use Livewire\Component;
 use App\Models\Subcategory;
 use Livewire\WithPagination;
 use Illuminate\Support\Facades\DB;
-//use Illuminate\Database\Eloquent\Builder;
 
 class Subcategories extends Component
 {
     use WithPagination;
 
-    public $search,$selected_id,$pageTitle,$componentName,$name,$categories,$category_id,$category_name;
+    public $pageTitle,$componentName,$search,$search_2,$selected_id;
+    public $name,$categories,$category_id,$category_name;
     private $pagination = 20;
 
     public function mount(){
@@ -21,10 +21,11 @@ class Subcategories extends Component
         $this->pageTitle = 'listado';
         $this->componentName = 'subcategorias';
         $this->search = '';
+        $this->search_2 = 0;
         $this->selected_id = 0;
         $this->name = '';
-        $this->categories = Category::select('id','name')->get();
-        $this->category_id = 'Elegir';
+        $this->categories = Category::select('id','name')->where('status_id',1)->get();
+        $this->category_id = 'elegir';
         $this->category_name = '';
         $this->resetValidation();
         $this->resetPage();
@@ -37,23 +38,67 @@ class Subcategories extends Component
 
     public function render()
     {
-        if(strlen($this->search) > 0)
+        switch ($this->search_2){
 
-            $data = Subcategory::with('category')
-            ->withCount('presentations')
-            ->where('name', 'like', '%' . $this->search . '%')
-            ->orWhereHas('category', function ($query) {
-                $query->where('name', 'like', '%' . $this->search . '%');
-            })
-            ->paginate($this->pagination);
+            case 0:
 
-        else
+                if(strlen($this->search) > 0){
 
-            $data = Subcategory::with('category')
-            ->withCount('presentations')
-            ->orderBy(Category::select('name')->whereColumn('categories.id','subcategories.category_id'))
-            ->paginate($this->pagination);
-            
+                    $data = Subcategory::with('category')
+                    ->where('status_id',1)
+                    ->where(function ($q1) {
+                        $q1->where('name', 'like', '%' . $this->search . '%');
+                        $q1->orWhere(function ($q2) {
+                            $q2->whereHas('category', function ($q3) {
+                                $q3->where('name', 'like', '%' . $this->search . '%');
+                            });
+                        });
+                    })
+                    ->orderBy(Category::select('name')->whereColumn('categories.id','subcategories.category_id'))
+                    ->orderBy('name')
+                    ->paginate($this->pagination);
+        
+                }else{
+        
+                    $data = Subcategory::with('category')
+                    ->where('status_id',1)
+                    ->orderBy(Category::select('name')->whereColumn('categories.id','subcategories.category_id'))
+                    ->orderBy('name')
+                    ->paginate($this->pagination);   
+                }
+
+            break;
+
+            case 1:
+
+                if(strlen($this->search) > 0){
+
+                    $data = Subcategory::with('category')
+                    ->where('status_id',2)
+                    ->where(function ($q1) {
+                        $q1->where('name', 'like', '%' . $this->search . '%');
+                        $q1->orWhere(function ($q2) {
+                            $q2->whereHas('category', function ($q3) {
+                                $q3->where('name', 'like', '%' . $this->search . '%');
+                            });
+                        });
+                    })
+                    ->orderBy(Category::select('name')->whereColumn('categories.id','subcategories.category_id'))
+                    ->orderBy('name')
+                    ->paginate($this->pagination);
+        
+                }else{
+        
+                    $data = Subcategory::with('category')
+                    ->where('status_id',2)
+                    ->orderBy(Category::select('name')->whereColumn('categories.id','subcategories.category_id'))
+                    ->orderBy('name')
+                    ->paginate($this->pagination);   
+                }
+
+            break;
+
+        }
 
         return view('livewire.subcategory.subcategories', ['subcategories' => $data])
         ->extends('layouts.theme.app')
@@ -65,29 +110,39 @@ class Subcategories extends Component
 
         $rules = [
 
-            'name' => 'required|unique:subcategories|min:3|max:100',
-            'category_id' => 'not_in:Elegir'
+            'name' => 'required|min:3|max:45|unique:subcategories',
+            'category_id' => 'not_in:elegir',
         ];
 
         $messages = [
 
             'name.required' => 'Campo requerido',
-            'name.unique' => 'Ya existe',
             'name.min' => 'Minimo 3 caracteres',
-            'name.max' => 'Maximo 100 caracteres',
-            'category_id.not_in' => 'Seleccione una opcion'
+            'name.max' => 'Maximo 45 caracteres',
+            'category_id.not_in' => 'Seleccione una opcion',
+            'name.unique' => 'Ya existe',
         ];
 
         $this->validate($rules, $messages);
 
-        Subcategory::create([
+        $subcategory = Subcategory::create([
 
             'name' => $this->name,
+            'status_id' => 1,
             'category_id' => $this->category_id
         ]);
 
-        $this->mount();
-        $this->emit('item-added', 'Registrado correctamente');
+        if($subcategory){
+
+            $this->emit('record-added', 'Registrado correctamente');
+            $this->mount();
+
+        }else{
+
+            $this->emit('record-error', 'Error al registrar');
+            return;
+        }
+
     }
 
     public function ShowCategoryModal(){
@@ -106,35 +161,44 @@ class Subcategories extends Component
 
         $rules = [
 
-            'category_name' => 'required|unique:categories,name|min:3|max:100'
+            'category_name' => 'required|min:3|max:45|unique:categories,name'
         ];
 
         $messages = [
 
             'category_name.required' => 'Campo requerido',
-            'category_name.unique' => 'Ya existe',
             'category_name.min' => 'Minimo 3 caracteres',
-            'category_name.max' => 'Maximo 100 caracteres',
+            'category_name.max' => 'Maximo 45 caracteres',
+            'category_name.unique' => 'Ya existe',
         ];
 
         $this->validate($rules, $messages);
 
-        Category::create([
+        $category = Category::create([
 
-            'name' => $this->category_name
+            'name' => $this->category_name,
+            'status_id' => 1
         ]);
 
-        $this->category_name = '';
-        $this->resetValidation($this->category_name = null);
-        $this->category_id = 'Elegir';
-        $this->categories = Category::select('id','name')->get();
-        $this->emit('item-added-2', 'Registrado correctamente');
-        $this->emit('show-modal', 'Mostrando modal');
+        if($category){
+
+            $this->category_name = '';
+            $this->resetValidation($this->category_name = null);
+            $this->categories = Category::select('id','name')->where('status_id',1)->get();
+            $this->category_id = $category->id;
+            $this->emit('record-added-2', 'Registrado correctamente');
+            $this->emit('show-modal', 'Mostrando modal');
+
+        }else{
+
+            $this->emit('record-error','Error al registrar');
+            return;
+        }
+
     }
 
     public function Edit(Subcategory $subcategory){
 
-        
         $this->name = $subcategory->name;
         $this->selected_id = $subcategory->id;
         $this->category_id = $subcategory->category->id;
@@ -145,17 +209,17 @@ class Subcategories extends Component
 
         $rules = [
 
-            'name' => "required|min:3|max:100|unique:subcategories,name,{$this->selected_id}",
-            'category_id' => 'not_in:Elegir'
+            'name' => "required|min:3|max:45|unique:subcategories,name,{$this->selected_id}",
+            'category_id' => 'not_in:elegir',
         ];
 
         $messages = [
 
             'name.required' => 'Campo requerido',
-            'name.unique' => 'Ya existe',
             'name.min' => 'Minimo 3 caracteres',
-            'name.max' => 'Maximo 100 caracteres',
-            'category_id.not_in' => 'Seleccione una opcion'
+            'name.max' => 'Maximo 45 caracteres',
+            'name.unique' => 'Ya existe',
+            'category_id.not_in' => 'Seleccione una opcion',
         ];
 
         $this->validate($rules, $messages);
@@ -168,16 +232,39 @@ class Subcategories extends Component
             'category_id' => $this->category_id
         ]);
 
+        $this->emit('record-updated', 'Actualizado correctamente');
         $this->mount();
-        $this->emit('item-updated', 'Actualizado correctamente');
     }
 
     protected $listeners = [
 
+        'activate' => 'Activate',
         'destroy' => 'Destroy'
     ];
 
-    public function Destroy(Subcategory $subcategory,$presentations_count)
+    public function Activate(Subcategory $subcategory,Category $category){
+
+        $subcategory->update([
+
+            'status_id' => 1
+
+        ]);
+
+        if($category->id != 1){
+
+            $category->update([
+
+                'status_id' => 1
+    
+            ]);
+
+        }
+
+        $this->emit('record-activated','Registro desbloqueado');
+        $this->mount();
+    }
+
+    /*public function Destroy(Subcategory $subcategory,$presentations_count)
     {
         
         if ($presentations_count > 0) {
@@ -188,10 +275,10 @@ class Subcategories extends Component
         } else {
 
             $subcategory->delete();
-            $this->emit('item-deleted', 'Eliminado correctamente');
+            $this->emit('record-deleted', 'Eliminado correctamente');
             $this->mount();
 
-            /*DB::beginTransaction();
+            DB::beginTransaction();
 
             try {
 
@@ -213,8 +300,20 @@ class Subcategories extends Component
 
                 DB::rollback();
                 throw $th;
-            }*/
+            }
         }
+    }*/
+
+    public function Destroy(Subcategory $subcategory){
+
+        $subcategory->update([
+
+            'status_id' => 2
+
+        ]);
+
+        $this->emit('record-deleted','Registro eliminado');
+        $this->mount();
     }
 
     public function resetUI(){
