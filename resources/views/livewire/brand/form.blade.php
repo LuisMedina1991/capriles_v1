19080246fb5442b7bd928458022aa01e7665a8fa
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-17 14:55:31 -0400

    formulario de marcas version final

--- resources/views/livewire/brand/form.blade.php
+++ resources/views/livewire/brand/form.blade.php
@@ -1,19 +1,22 @@
 @include('common.modalHead')
 
-<div class="row">
-    <div class="col-sm-12">
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text">
-                    <span class="fas fa-edit"></span>
-                </span>
+    <div class="row">
+        <div class="col-sm-12">
+            <label><b>Nombre*</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="name" class="form-control component-name" placeholder="Nombre para la marca...">
+                </div>
+                @error('name')
+                    <span class="text-danger er">{{ $message }}</span>
+                @enderror
             </div>
-            <input type="text" wire:model.lazy="name" class="form-control component-name" placeholder="Nombre para la marca...">
         </div>
-        @error('name')
-            <span class="text-danger er">{{ $message }}</span>
-        @enderror
     </div>
-</div>
 
 @include('common.modalFooter')
