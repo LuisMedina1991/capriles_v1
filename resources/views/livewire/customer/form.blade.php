86a751b9df4c7232a70fee1d13f8f75bc0deb647
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-23 11:58:10 -0400

    Formulario de clientes version final

--- resources/views/livewire/customer/form.blade.php
+++ resources/views/livewire/customer/form.blade.php
@@ -1,120 +1,103 @@
 @include('common.modalHead')
-<div class="row">
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Cliente</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="name" class="form-control component-name"
-                    placeholder="Nombre para el cliente...">
-            </div>
-            @error('name')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Alias</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="alias" class="form-control" placeholder="Alias para el cliente...">
-            </div>
-            @error('alias')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Telefono</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="number" wire:model.lazy="phone" class="form-control" placeholder="Telefono para el cliente...">
-            </div>
-            @error('phone')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Email</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="email" class="form-control" placeholder="Email para el cliente...">
-            </div>
-            @error('email')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Ciudad</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="city" class="form-control" placeholder="Ciudad para el cliente...">
-            </div>
-            @error('city')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="form-group">
-            <label><b>Pais</b></label>
-            <div class="input-group">
-                <div class="input-group-prepend">
-                    <span class="input-group-text">
-                        <span class="fas fa-edit"></span>
-                    </span>
-                </div>
-                <input type="text" wire:model.lazy="country" class="form-control" placeholder="Pais para el cliente...">
-            </div>
-            @error('country')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
-        </div>
-    </div>
 
-    @if($selected_id > 0 && $search_2 > 0)
-
-        <div class="col-sm-6">
+    <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Cliente*</b></label>
             <div class="form-group">
-                <label><b>Estado del registro</b></label>
-                <select wire:model="status_id" class="form-control text-uppercase">
-                    <option value="elegir">elegir</option>
-                    <option value="1">activo</option>
-                    <option value="2">bloqueado</option>
-                </select>
-                @error('status_id')
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="name" class="form-control component-name"
+                        placeholder="Nombre para el cliente...">
+                </div>
+                @error('name')
                 <span class="text-danger er">{{ $message }}</span>
                 @enderror
             </div>
         </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Alias*</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="alias" class="form-control" placeholder="Alias para el cliente...">
+                </div>
+                @error('alias')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Telefono</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="number" wire:model.lazy="phone" class="form-control" placeholder="Telefono para el cliente...">
+                </div>
+                @error('phone')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Email</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="email" class="form-control" placeholder="Email para el cliente...">
+                </div>
+                @error('email')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Ciudad</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="city" class="form-control" placeholder="Ciudad para el cliente...">
+                </div>
+                @error('city')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Pais</b></label>
+            <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">
+                            <span class="fas fa-edit"></span>
+                        </span>
+                    </div>
+                    <input type="text" wire:model.lazy="country" class="form-control" placeholder="Pais para el cliente...">
+                </div>
+                @error('country')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
+            </div>
+        </div>
+    </div>
 
-    @endif
-
-</div>
 @include('common.modalFooter')
