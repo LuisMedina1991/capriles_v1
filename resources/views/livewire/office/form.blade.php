c1f640f3c5cd9bc42572fe804e8ebfb28239e92e
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-23 12:33:00 -0400

    Formulario de sucursales version final

--- resources/views/livewire/office/form.blade.php
+++ resources/views/livewire/office/form.blade.php
@@ -2,9 +2,9 @@
 
     <div class="row">
 
-        <div class="col-sm-6">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Nombre*</b></label>
             <div class="form-group">
-                <label><b>Nombre</b></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text">
@@ -14,15 +14,15 @@
                     <input type="text" wire:model.lazy="name" class="form-control component-name"
                         placeholder="Nombre para la sucursal...">
                 </div>
+                @error('name')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
             </div>
-            @error('name')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
         </div>
 
-        <div class="col-sm-6">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Alias*</b></label>
             <div class="form-group">
-                <label><b>Alias</b></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text">
@@ -32,15 +32,15 @@
                     <input type="text" wire:model.lazy="alias" class="form-control"
                         placeholder="Alias para la sucursal...">
                 </div>
+                @error('alias')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
             </div>
-            @error('alias')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
         </div>
 
-        <div class="col-sm-6">
+        <div class="col-sm-12 col-md-12 col-lg-6">
+            <label><b>Telefono</b></label>
             <div class="form-group">
-                <label><b>Telefono</b></label>
                 <div class="input-group">
                     <div class="input-group-prepend">
                         <span class="input-group-text">
@@ -50,46 +50,20 @@
                     <input type="number" wire:model.lazy="phone" class="form-control"
                         placeholder="Telefono para la sucursal...">
                 </div>
+                @error('phone')
+                <span class="text-danger er">{{ $message }}</span>
+                @enderror
             </div>
-            @error('phone')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
         </div>
 
-        @if($selected_id > 0 && $search_2 > 0)
-
-            <div class="col-sm-6">
-                <div class="form-group">
-                    <label><b>Estado del registro</b></label>
-                    <select wire:model="status_id" class="form-control text-uppercase">
-                        <option value="elegir">elegir</option>
-                        <option value="1">activo</option>
-                        <option value="2">bloqueado</option>
-                    </select>
-                    @error('status_id')
-                    <span class="text-danger er">{{ $message }}</span>
-                    @enderror
-                </div>
-            </div>
-
-        @endif
-
         <div class="col-sm-12">
+            <label><b>Direccion</b></label>
             <div class="form-group">
-                <label><b>Direccion</b></label>
-                <div class="input-group">
-                    <div class="input-group-prepend">
-                        <span class="input-group-text">
-                            <span class="fas fa-edit"></span>
-                        </span>
-                    </div>
-                    <input type="text" wire:model.lazy="address" class="form-control"
-                        placeholder="Direccion para la sucursal...">
-                </div>
+                <textarea wire:model.lazy="address" class="form-control" placeholder="Direccion para la sucursal..." cols="30" rows="2"></textarea>
+                @error('address')
+                    <span class="text-danger er">{{ $message }}</span>
+                @enderror
             </div>
-            @error('address')
-            <span class="text-danger er">{{ $message }}</span>
-            @enderror
         </div>
 
     </div>
