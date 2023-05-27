3f37d93c6568d8230c8c541ccc62e5f6fbcd3af0
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-27 10:38:14 -0400

    vista principal de contenedores casi completa solo faltan algunas relaciones

--- resources/views/livewire/container/containers.blade.php
+++ resources/views/livewire/container/containers.blade.php
@@ -8,25 +8,31 @@
                 <ul class="tabs tab-pills">
                     <li>
                         <a href="javascript:void(0)" class="btn bg-dark" data-toggle="modal"
-                            data-target="#theModal" title="Nuevo Contenedor">Agregar</a>
+                            data-target="#theModal">Agregar</a>
                     </li>
                 </ul>
             </div>
 
-            <div class="row mb-4">
-                <div class="col-sm-3">
-                    <select id="search" wire:model="search" class="form-control text-uppercase">
-                        <option value="Elegir">filtro por categoria</option>
-                        @foreach ($categories as $categorie)
-                        <option value="{{$categorie->id}}">{{$categorie->name}}</option>
-                        @endforeach
-                    </select>
+            <div class="row">
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <h6><b>Filtro por categoria</b></h6>
+                    <div class="form-group">
+                        <select id="search" wire:model="search" class="form-control text-uppercase">
+                            <option value="elegir">elegir</option>
+                            @foreach ($categories as $categorie)
+                            <option value="{{$categorie->id}}">{{$categorie->name}}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <div class="col-sm-3">
-                    <select id="search_2" wire:model="search_2" class="form-control text-uppercase {{$search_2 == 0 ? 'badge-success' : 'badge-danger'}}">
-                        <option value="0">contenedores activos</option>
-                        <option value="1">contenedores bloqueados</option>
-                    </select>
+                <div class="col-sm-12 col-md-6 col-lg-3">
+                    <h6><b>Estado del registro</b></h6>
+                    <div class="form-group">
+                        <select id="search_2" wire:model="search_2" class="form-control">
+                            <option value="0">Activo</option>
+                            <option value="1">Bloqueado</option>
+                        </select>
+                    </div>
                 </div>
             </div>
 
@@ -35,9 +41,8 @@
                     <table class="table table-striped table-bordered mt-1">
                         <thead class="text-white" style="background: #3B3F5C">
                             <tr>
-                                <th class="table-th text-white text-center">estado</th>
                                 <th class="table-th text-white text-center">prefijo</th>
-                                <th class="table-th text-white text-center">productos relacionados</th>
+                                {{--<th class="table-th text-white text-center">productos relacionados</th>--}}
                                 <th class="table-th text-white text-center">categoria</th>
                                 <th class="table-th text-white text-center">subcategoria</th>
                                 <th class="table-th text-white text-center">presentacion</th>
@@ -49,19 +54,11 @@
                             @foreach ($containers as $container)
                             <tr>
                                 <td>
-                                    <h6 class="text-center text-uppercase">
-                                        <span
-                                            class="badge text-uppercase {{$container->status->name == 'activo' ? 'badge-success' : 'badge-danger'}}">
-                                            {{$container->status->name}}
-                                        </span>
-                                    </h6>
-                                </td>
-                                <td>
                                     <h6 class="text-center text-uppercase">{{ $container->prefix }}</h6>
                                 </td>
-                                <td>
+                                {{--<td>
                                     <h6 class="text-center text-uppercase">{{ $container->active_products_count }}</h6>
-                                </td>
+                                </td>--}}
                                 <td class="text-center text-uppercase">
                                     <h6 class="text-center text-uppercase">{{ $container->subcategory->category->name }}</h6>
                                 </td>
@@ -75,23 +72,30 @@
                                     <h6 class="text-center text-uppercase">{{ $container->additional_info }}</h6>
                                 </td>
                                 <td class="text-center">
-                                    <a href="javascript:void(0)"
-                                        wire:click="Edit({{$container->id}})"
-                                        class="btn btn-dark mtmobile" title="Editar">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <a href="javascript:void(0)"
-                                        onclick="Confirm('{{$container->id}}','{{$container->active_products_count}}')"
-                                        class="btn btn-dark" title="Eliminar">
-                                        <i class="fas fa-trash"></i>
-                                    </a>
+                                    @if($search_2 == 0)
+                                        <a href="javascript:void(0)" wire:click="Edit({{$container->id}})"
+                                            class="btn btn-dark" title="Editar">
+                                            <i class="fas fa-edit"></i>
+                                        </a>
+                                        <a href="javascript:void(0)"
+                                            onclick="Confirm_1('{{$container->id}}')"
+                                            class="btn btn-dark" title="Eliminar">
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+                                    @else
+                                        <a href="javascript:void(0)"
+                                            onclick="Confirm_2('{{$container->id}}')"
+                                            class="btn btn-dark" title="Activar">
+                                            <i class="fas fa-check"></i>
+                                        </a>
+                                    @endif
                                 </td>
                             </tr>
                             @endforeach
                         </tbody>
                     </table>
 
-                    {{$containers->links()}}
+                    {{ $containers->links() }}
 
                 </div>
             </div>
@@ -99,7 +103,6 @@
     </div>
 
     @include('livewire.container.form')
-    @include('livewire.container.form_2')
     @include('livewire.container.category_form')
     @include('livewire.container.presentation_form')
     @include('livewire.container.subcategory_form')
@@ -113,15 +116,15 @@
         window.livewire.on('show-modal', msg=>{
             $('#theModal').modal('show')
         });
-        window.livewire.on('item-added', msg=>{
+        window.livewire.on('record-added', msg=>{
             $('#theModal').modal('hide')
             noty(msg)
         });
-        window.livewire.on('show-edit-modal', msg=>{
-            $('#edit_modal').modal('show')
+        window.livewire.on('record-updated', msg=>{
+            $('#theModal').modal('hide')
+            noty(msg)
         });
-        window.livewire.on('item-updated', msg=>{
-            $('#edit_modal').modal('hide')
+        window.livewire.on('record-activated', msg=>{
             noty(msg)
         });
         window.livewire.on('show-modal-2', msg=>{
@@ -131,7 +134,7 @@
         $('#category_modal').on('shown.bs.modal', function(e){
             $('.component-name').focus()
         });
-        window.livewire.on('item-added-2', msg=>{
+        window.livewire.on('record-added-2', msg=>{
             $('#category_modal').modal('hide')
             noty(msg)
         });
@@ -139,10 +142,10 @@
             $('#theModal').modal('hide')
             $('#subcategory_modal').modal('show')
         });
-        $('#subcategory_form').on('shown.bs.modal', function(e){
+        $('#subcategory_modal').on('shown.bs.modal', function(e){
             $('.component-name').focus()
         });
-        window.livewire.on('item-added-3', msg=>{
+        window.livewire.on('record-added-3', msg=>{
             $('#subcategory_modal').modal('hide')
             $('#theModal').modal('show')
             noty(msg)
@@ -154,7 +157,7 @@
         $('#presentation_modal').on('shown.bs.modal', function(e){
             $('.component-name').focus()
         });
-        window.livewire.on('item-added-4', msg=>{
+        window.livewire.on('record-added-4', msg=>{
             $('#presentation_modal').modal('hide')
             $('#theModal').modal('show')
             noty(msg)
@@ -163,15 +166,15 @@
             $('#subcategory_modal').modal('hide')
             $('#category_modal').modal('show')
         });
-        window.livewire.on('item-deleted', msg=>{
+        window.livewire.on('record-deleted', msg=>{
             noty(msg)
         });
-        window.livewire.on('item-error', msg=>{
+        window.livewire.on('record-error', msg=>{
             noty(msg,2)
         });        
     });
 
-    function Confirm(container_id,active_products_count){
+    function Confirm_1(container_id,active_products_count){
 
         if(active_products_count > 0){
 
@@ -196,6 +199,29 @@
             if(result.value){
 
                 window.livewire.emit('destroy', container_id,active_products_count)
+                swal.close()
+            }
+        })
+    }
+
+    function Confirm_2(id){
+
+        swal({
+
+            title: 'CONFIRMAR',
+            text: '¿CONFIRMA ACTIVAR EL REGISTRO?',
+            type: 'warning',
+            showCancelButton: true,
+            cancelButtonText: 'CERRAR',
+            cancelButtonColor: '#fff',
+            confirmButtonColor: '#3B3F5C',
+            confirmButtonText: 'ACEPTAR'
+
+        }).then(function(result){
+
+            if(result.value){
+                
+                window.livewire.emit('activate',id)
                 swal.close()
             }
         })
