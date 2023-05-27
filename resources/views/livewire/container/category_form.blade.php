25b1c667f59f98a5d44e8e6a2d052c41d805b284
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-27 10:41:05 -0400

    formulario de categorias dentro de contenedores version final

--- resources/views/livewire/container/category_form.blade.php
+++ resources/views/livewire/container/category_form.blade.php
@@ -3,25 +3,28 @@
         <div class="modal-content">
             <div class="modal-header bg-dark">
                 <h5 class="modal-title text-white text-uppercase">
-                    <b>crear | categoria</b>
+                    <b>crear | categorias</b>
                 </h5>
                 <h6 class="text-center text-warning" wire:loading>POR FAVOR ESPERE</h6>
             </div>
             <div class="modal-body">
                 <div class="row">
                     <div class="col-sm-12">
-                        <div class="input-group">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">
-                                    <span class="fas fa-edit"></span>
-                                </span>
+                        <label><b>Nombre*</b></label>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                        <span class="fas fa-edit"></span>
+                                    </span>
+                                </div>
+                                <input type="text" wire:model.lazy="category_name" class="form-control component-name"
+                                    placeholder="Nombre para la categoria...">
                             </div>
-                            <input type="text" wire:model.lazy="category_name" class="form-control component-name"
-                                placeholder="Nombre para la categoria...">
+                            @error('category_name')
+                            <span class="text-danger er">{{ $message }}</span>
+                            @enderror
                         </div>
-                        @error('category_name')
-                        <span class="text-danger er">{{ $message }}</span>
-                        @enderror
                     </div>
                 </div>
             </div>
