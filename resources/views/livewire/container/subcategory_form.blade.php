39c8a90bc0cea1f781442fa5a39dfa74f61f92be
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-27 10:41:55 -0400

    formulario de subcategorias dentro de contenedores version final

--- resources/views/livewire/container/subcategory_form.blade.php
+++ resources/views/livewire/container/subcategory_form.blade.php
@@ -3,15 +3,15 @@
         <div class="modal-content">
             <div class="modal-header bg-dark">
                 <h5 class="modal-title text-white text-uppercase">
-                    <b>crear | subcategoria</b>
+                    <b>crear | subcategorias</b>
                 </h5>
                 <h6 class="text-center text-warning" wire:loading>POR FAVOR ESPERE</h6>
             </div>
             <div class="modal-body">
                 <div class="row">
                     <div class="col-sm-12">
+                        <label><b>Nombre*</b></label>
                         <div class="form-group">
-                            <label><b>Subcategoria</b></label>
                             <div class="input-group">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text">
@@ -21,37 +21,41 @@
                                 <input type="text" wire:model.lazy="subcategory_name" class="form-control component-name"
                                     placeholder="Nombre para la subcategoria...">
                             </div>
-                        </div>
-                        @error('subcategory_name')
-                        <span class="text-danger er">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div>
-                    <label><b>Categoria</b></label>
-                    <div class="d-flex">
-                        <div class="w-75">
-                            <select wire:model="categoryId" class="form-control text-uppercase">
-                                <option value="Elegir">Elegir</option>
-                                @foreach ($categories as $category)
-                                <option value="{{$category->id}}">{{$category->name}}</option>
-                                @endforeach
-                            </select>
-                            @error('categoryId')
+                            @error('subcategory_name')
                             <span class="text-danger er">{{ $message }}</span>
                             @enderror
                         </div>
-                        <div class="ml-auto">
-                            <button type="button" wire:click.prevent="ShowCategoryModal({{$modal_id}})" class="btn btn-success">Añadir Categoria</button>
+                    </div>
+                </div>
+                <div>
+                    <label><b>Categoria*</b></label>
+                    <div class="row">
+                        <div class="col-sm-12 col-md-9 col-lg-9">
+                            <div class="form-group">
+                                <select wire:model="categoryId" class="form-control text-uppercase">
+                                    <option value="elegir">elegir</option>
+                                    @foreach ($categories as $category)
+                                    <option value="{{$category->id}}">{{$category->name}}</option>
+                                    @endforeach
+                                </select>
+                                @error('categoryId')
+                                <span class="text-danger er">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-sm-12 col-md-3 col-lg-3">
+                            <div class="form-group">
+                                <button type="button" wire:click.prevent="ShowCategoryModal({{$modal_id}})" class="btn btn-success" title="Nueva Categoria">Añadir Nuevo</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer">
                 <button type="button" wire:click.prevent="StoreSubcategory()"
-                    class="btn btn-dark close-modal">GUARDAR</button>
+                    class="btn btn-dark close-modal text-uppercase">guardar</button>
                 <button type="button" wire:click.prevent="CloseSubcategoryModal()"
-                    class="btn btn-dark close-btn text-info" data-dismiss="modal">CERRAR</button>
+                    class="btn btn-dark close-btn text-info text-uppercase" data-dismiss="modal">cerrar</button>
             </div>
         </div>
     </div>
