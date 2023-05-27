3d8cedbc2b2822d98d7d333268b89894e45998fc
LuisMedina1991 <luchito19medina91@gmail.com>
2023-05-27 10:34:24 -0400

    migracion final para contenedores

---
+++ database/migrations/2019_12_14_000013_create_presentation_subcategory_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresentationSubcategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presentation_subcategory', function (Blueprint $table) {
+            $table->id();
+            $table->string('prefix',15)->unique();
+            $table->string('additional_info',45)->nullable();
+            $table->foreignId('presentation_id')->constrained();
+            $table->foreignId('subcategory_id')->constrained();
+            $table->foreignId('status_id')->constrained();
+            /*$table->unsignedBigInteger('subcategory_id');
+            $table->unsignedBigInteger('presentation_id');
+            $table->foreign('subcategory_id')->references('id')->on('subcategories');
+            $table->foreign('presentation_id')->references('id')->on('presentations');*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presentation_subcategory');
+    }
+}
