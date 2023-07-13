ce52c397c64add3ac24e582a1fe6717a8419d0ab
LuisMedina1991 <luchito19medina91@gmail.com>
2023-07-13 14:47:29 -0400

    adicion de migracion de tabla values con campos cost y price como enteros

---
+++ database/migrations/2019_12_14_000015_create_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('values', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cost');
+            $table->integer('price');
+            $table->foreignId('product_id')->constrained();
+            $table->foreignId('status_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('values');
+    }
+}
