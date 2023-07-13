bc1af6bc4c3c429477cd5ad196e42ea730de746b
LuisMedina1991 <luchito19medina91@gmail.com>
2023-07-13 14:43:16 -0400

    adicion de relacion con tabla statuses y products, accesor y mutator para atributos cost y price, sin relacion con offices aun

--- app/Models/Value.php
+++ app/Models/Value.php
@@ -12,25 +12,54 @@ class Value extends Model
     
     protected $fillable = ['cost','price','product_id','status_id'];
 
-    
+
+    //relacion muchos a uno con products
     public function product(){
 
         return $this->belongsTo(Product::class);
     }
 
-    
+    //relacion muchos a uno con statuses
     public function status(){
 
         return $this->belongsTo(Status::class);
     }
 
-    
+    /*//relacion muchos a muchos con offices
     public function offices()
     {
         return $this->belongsToMany(Office::class)
         ->withPivot(['id', 'stock', 'alerts'])
         ->withTimestamps()
         ->using(OfficeValue::class);
+    }*/
+
+    /*los siguientes son accesors y mutators para manipular campos en la tabla*/
+    /*los campos cost y price se deben almacenar como enteros en la db pero deben ser decimales al manipularlos*/
+    /*por lo que al obtenerlos su valor se divide entre 100 y al almacenarlos su valor se multiplica por 100*/
+
+    //accesor para el atributo cost
+    public function getCostAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    //mutator para el atributo cost
+    public function setCostAttribute($value)
+    {
+        $this->attributes['cost'] = $value * 100;
+    }
+
+    //accesor para el atributo price
+    public function getPriceAttribute($value)
+    {
+        return $value / 100;
+    }
+
+    //mutator para el atributo price
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['price'] = $value * 100;
     }
 
 }
